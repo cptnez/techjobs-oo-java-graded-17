4b34a66084eec991bb8361903464af5753327f04
cptnez <gtracey24@gmail.com>
2024-05-03 10:24:48 -0500

    Commit prior to starting Task 6.

--- src/main/java/org/launchcode/techjobs/oo/PositionType.java
+++ src/main/java/org/launchcode/techjobs/oo/PositionType.java
@@ -24,6 +24,9 @@ public class PositionType {
         return value;
     }
 
+    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
+    //  their id fields match.
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -36,22 +39,6 @@ public class PositionType {
     public int hashCode() {
         return Objects.hash(id, value);
     }
-
-    // TODO: Add custom equals and hashCode methods. Consider two PositionType objects "equal" when
-    //  their id fields match.
-
-//    @Override
-//    public boolean equals(Object o) {  // Two objects are equal if they have the same id.
-//        if (this == o) return true;
-//        if (!(o instanceof PositionType)) return false;
-//        PositionType position = (PositionType) o;
-//        return getId() == position.getId();
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(getId());
-//    }
 
     // Getters and Setters:
 
